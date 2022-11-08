1ab847a37922fd368e33ea7dc211247eabb5acc0
vic-it <victor.uelger@web.de>
2022-11-08 17:13:36 +0100

    small restructure

--- src/main.go
+++ src/main.go
@@ -4,11 +4,13 @@ import (
 	"github.com/vic-it/OSM-FMI/src/backend/util"
 )
 
-func readFile(path string) {
-	util.ReadPBF(path)
+func fetchWorld(path string) {
+	//loads whole osm.pbf world from filepath into basic format
+	//basicWorld := util.PBFtoBASIC(path)
+	util.PBFtoBASIC(path)
 }
 
 func main() {
 	PBFpath := "../../data/antarctica.osm.pbf"
-	readFile(PBFpath)
+	fetchWorld(PBFpath)
 }

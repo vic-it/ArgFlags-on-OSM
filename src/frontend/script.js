5b57f4181d918bf07a05d74cd28428c0220d9941
vic-it <victor.uelger@web.de>
2022-11-23 18:43:55 +0100

    add js console prints for clicks

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,13 +1,14 @@
 var isStart = true;
+//maybe store node IDs too in start dest objects
 var start = {marker: null, lon: 0, lat: 0}
 var dest = {marker: null, lon: 0, lat: 0}
 
 
 function clickHandler(event){
-    console.log(event)
     let url = new URL("http://localhost:8080/getpoint");
     url.searchParams.append("lon", event.latlng.lng);
     url.searchParams.append("lat", event.latlng.lat);
+    console.log("clicked point (lon/lat): \n["+event.latlng.lng.toFixed(6)+", "+event.latlng.lat.toFixed(6)+"]")
     
     //fetches result
     fetch(url).then((response) => {
@@ -19,6 +20,9 @@ function clickHandler(event){
         coordinates = result.split("x")
         //0 lon, 1 lat, the + before the coordinates casts the string valued coordinates into number values
         //i hate javascript
+        
+        console.log("closest grid point (lon/lat): \n["+coordinates[0]+", "+coordinates[1]+"]")
+        console.log("-----------------")
         addMarker(+coordinates[0],+coordinates[1])
     })
 }
@@ -30,7 +34,7 @@ function addMarker(lon, lat){
         if(start.marker != null){
         map.removeLayer(start.marker)
         }
-        start.marker = L.marker([lat, lon])
+        start.marker = L.marker([lat, lon]).bindPopup("start")
         start.lon = lon
         start.lat = lat
         start.marker.addTo(map)
@@ -38,7 +42,7 @@ function addMarker(lon, lat){
         if(dest.marker !=null){
         map.removeLayer(dest.marker)
         }
-        dest.marker = L.marker([lat, lon])
+        dest.marker = L.marker([lat, lon]).bindPopup("destination")
         dest.lon = lon
         dest.lat = lat
         dest.marker.addTo(map)
@@ -55,7 +59,7 @@ function calculateRoute(){
 // https://www.youtube.com/watch?v=Fk-P5l7DJjo
 let mapOpions = {
     center: [0, 0],
-    zoom: 5
+    zoom: 2
 }
 var map = L.map('map', mapOpions);
 let layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')

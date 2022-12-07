42f631587234f952310ec962faa5e8f3746dc32d
vic-it <victor.uelger@web.de>
2022-12-07 15:41:04 +0100

    add routing and more frontend functionality
    backend to follow

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,7 +1,8 @@
 var isStart = true;
 //maybe store node IDs too in start dest objects
-var start = {marker: null, lon: 0, lat: 0}
-var dest = {marker: null, lon: 0, lat: 0}
+var start = {marker: null, lon: 0, lat: 0, id: -1}
+var dest = {marker: null, lon: 0, lat: 0, id: -1}
+var path = null
 
 
 function clickHandler(event){
@@ -21,36 +22,91 @@ function clickHandler(event){
         //0 lon, 1 lat, the + before the coordinates casts the string valued coordinates into number values
         //i hate javascript
         
-        console.log("closest grid point (lon/lat): \n["+coordinates[0]+", "+coordinates[1]+"]")
+        console.log("closest grid point (lon/lat): \n["+coordinates[0]+", "+coordinates[1]+"] id = "+coordinates[2])
         console.log("-----------------")
-        addMarker(+coordinates[0],+coordinates[1])
+        addMarker(+coordinates[0],+coordinates[1], +coordinates[2])
     })
 }
 
+function routeHandler(){
+    srcID = start.id
+    destID = dest.id
+    if(start.id <0 || dest.id<0){
+        return
+    }
+    
+    let url = new URL("http://localhost:8080/getroute");
+    url.searchParams.append("src", srcID);
+    url.searchParams.append("dest", destID);
+
+    fetch(url).then((response) => {
+        //extracts the response
+        answer = response.text()
+        return answer
+    }).then((result) => {
+        //takes body of response and splits it into the coordinates lon then lat
+        distCoords = result.split("y")
+        if (+distCoords[0] <0){
+            console.log("NO PATH FOUND!")
+        } else{
+            distance = +distCoords[0]
+            rawCoordinates = distCoords[1].split("x")
+            var coordinates = [];
+            for (const coord in rawCoordinates) {
+                lat = +coord.split(z)[1]
+                lon = +coord.split(z)[0]
+                coordinates.append([lat, lon])
+            }
+            
+            document.getElementById("distance").value = ""+distance+"m"
+            addRoute(coordinates)
+            console.log("Path found with distance: "+distance+"m")
+        }
+        //0 lon, 1 lat, the + before the coordinates casts the string valued coordinates into number values
+        //i hate javascript
+        
+        console.log("closest grid point (lon/lat): \n["+coordinates[0]+", "+coordinates[1]+"] id = "+coordinates[2])
+        console.log("-----------------")
+        addMarker(+coordinates[0],+coordinates[1], +coordinates[2])
+    })
+}
+
+//coordinates in lat-longs
+function addRoute(coordinates){
+    if(path !=null){
+        map.removeLayer(path)
+    }    
+    path = L.polyline(coordinates, {color: 'blue'}).addTo(map)
+}
 
 // type 0 -> starting node, type 1 -> destination node
-function addMarker(lon, lat){
+function addMarker(lon, lat, id){
     if(isStart){
         if(start.marker != null){
         map.removeLayer(start.marker)
         }
-        start.marker = L.marker([lat, lon]).bindPopup("start")
+        start.marker = L.marker([lat, lon]).bindPopup("Source-ID: "+id+" | Lon: "+lon+" | Lat: "+lat)
         start.lon = lon
         start.lat = lat
+        start.id = id
         start.marker.addTo(map)
+        document.getElementById("idField1").value = id
+        document.getElementById("lonField1").value = lon
+        document.getElementById("latField1").value = lat
     } else {
         if(dest.marker !=null){
         map.removeLayer(dest.marker)
         }
-        dest.marker = L.marker([lat, lon]).bindPopup("destination")
+        dest.marker = L.marker([lat, lon]).bindPopup("Dest.-ID: "+id+" | Lon: "+lon+" | Lat: "+lat)
         dest.lon = lon
         dest.lat = lat
+        dest.id = id
         dest.marker.addTo(map)
+        document.getElementById("idField2").value = id
+        document.getElementById("lonField2").value = lon
+        document.getElementById("latField2").value = lat
     }
     isStart =! isStart
-}
-function calculateRoute(){
-
 }
 
 
@@ -65,3 +121,7 @@ var map = L.map('map', mapOpions);
 let layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')
 layer.addTo(map)
 map.on("click", clickHandler)
+//add "usable" box
+var bounds = [[-90, -180], [90, 180]];
+L.rectangle(bounds, {color: "#aaffaa", weight: 3, fillOpacity: 0}).addTo(map)
+map.fitBounds(bounds)
--- src/main.go
+++ src/main.go
@@ -14,7 +14,7 @@ var graph util.Graph
 
 func main() {
 	println("\nStart!")
-	randomTestFunction(5000)
+	randomTestFunction(100000)
 	startServer()
 }
 
@@ -22,7 +22,7 @@ func startServer() {
 	println("starting http server...")
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
 	http.HandleFunc("/getpoint", getPointHandler())
-	http.HandleFunc("/calcRoute", getRouteHandler())
+	http.HandleFunc("/getroute", getRouteHandler())
 	http.HandleFunc("/directory", getDirectoryHandler())
 	http.HandleFunc("/preprocess", getPreprocessingHandler())
 	http.ListenAndServe(":8080", nil)
@@ -35,10 +35,10 @@ func getPointHandler() http.HandlerFunc {
 		inputLat, _ := strconv.ParseFloat(urlQuery["lat"][0], 64)
 
 		fmt.Printf("click lon: %f\n", inputLon)
-		fmt.Printf("click lat: %f\n--\n", inputLat)
+		fmt.Printf("click lat: %f\n", inputLat)
 		nodeID := util.GetClosestGridNode(inputLon, inputLat, graph)
 
-		outputString := fmt.Sprintf("%fx%f", graph.Nodes[nodeID][0], graph.Nodes[nodeID][1])
+		outputString := fmt.Sprintf("%fx%fx%d", graph.Nodes[nodeID][0], graph.Nodes[nodeID][1], nodeID)
 		writer.WriteHeader(http.StatusOK)
 		writer.Write([]byte(outputString))
 	}
@@ -49,11 +49,11 @@ func getPointHandler() http.HandlerFunc {
 func getRouteHandler() http.HandlerFunc {
 	routingHandler := func(writer http.ResponseWriter, request *http.Request) {
 		urlQuery := request.URL.Query()
-		processType, _ := strconv.ParseFloat(urlQuery["process"][0], 64)
-		processOption, _ := strconv.ParseFloat(urlQuery["option"][0], 64)
+		src, _ := strconv.ParseInt(urlQuery["src"][0], 0, 64)
+		dest, _ := strconv.ParseInt(urlQuery["dest"][0], 0, 64)
 
-		fmt.Printf("process type: %f\n", processType)
-		fmt.Printf("option: %f\n--\n", processOption)
+		fmt.Printf("source node ID: %d\n", src)
+		fmt.Printf("destination node ID: %d\n--\n", dest)
 
 		outputString := "lol hier text"
 		writer.WriteHeader(http.StatusOK)
